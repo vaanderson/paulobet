2feb8d671737f5a05d61e1aa605a0d03d2d197db
Vanderson Soares <vanderson.stefanio@hotmail.com>
2022-11-18 16:03:35 -0300

    ajustes

--- src/components/editor/EditorToolbar.tsx
+++ src/components/editor/EditorToolbar.tsx
@@ -26,10 +26,10 @@ const FONT_SIZE = [
 ];
 const HEADINGS = ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5', 'Heading 6'];
 
-export function undoChange() {
+export function undoChange(this: any) {
   this.quill.history.undo();
 }
-export function redoChange() {
+export function redoChange(this: any) {
   this.quill.history.redo();
 }
 
